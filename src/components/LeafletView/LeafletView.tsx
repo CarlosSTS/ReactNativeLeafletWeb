cb2b3849074b72a23b83cc159084336c7a464fe9
Carlos Ferreira <63306370+CarlosSTS@users.noreply.github.com>
2026-03-26 09:31:07 -0300

    fix: export LeafletView

---
+++ src/components/LeafletView/LeafletView.tsx
@@ -0,0 +1,4 @@
+// TODO: Add platform-specific implementations for LeafletView and export them from this file. 
+// Avoid the error "File 'src/components/LeafletView/LeafletView.tsx' is not a module.'"
+
+export { LeafletView } from './LeafletView.web';
